ded7dbaa03c8240a14b4943dd8345f54dc5b0ca5
IshanJawade <ishankjawade@gmail.com>
2024-10-18 14:01:58 -0700

    changing bits modified for turning ON and OFF

--- bitwise/changing_bits.c
+++ bitwise/changing_bits.c
@@ -10,9 +10,17 @@ void print_binary(unsigned int n){
 
 int main (){
 	unsigned int REG = 0b00100010;
-	int BIT = 3;
-	REG |= (1 << BIT);	// insert 1 in 3rd position
 	printf("Current state of REG : ", REG);
 	print_binary(REG);
+
+	int BIT = 3;
+	REG |= (1 << BIT);	// insert 1 in 3rd position (turning on)
+	printf("Current state of REG : ", REG);
+	print_binary(REG);
+
+	REG &= ~(1 << BIT); // insert 0 in 3rd position (turning off)
+	printf("Current state of REG : ", REG);
+	print_binary(REG);
+
 	return 0;
 }
