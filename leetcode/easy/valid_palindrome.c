8e420d8499b0292f35444bbb2ae7843412000a96
IshanJawade <ishankjawade@gmail.com>
2025-08-14 16:21:41 -0700

    valid panlindrome code added

---
+++ leetcode/easy/valid_palindrome.c
@@ -0,0 +1,37 @@
+# include <stdio.h>
+# include <stdbool.h>
+# include <strings.h>
+# include <ctype.h> 
+
+bool is_valid_Palindrome(char s[]){
+	int s_len = strlen(s);
+
+	// Remove special characters, numbers, and convert to lowercase
+	char final_string[s_len +1];  
+    int counter = 0;
+
+    for (int i = 0; i < s_len; i++) {
+        if (isalnum(s[i])) { 
+            final_string[counter] = tolower(s[i]); // Convert to lowercase
+            counter++;
+        }
+    }
+    final_string[counter] = '\0'; 
+
+	// to reverse a string 
+	char rev_string[counter +1];
+	for (int i = 0; i < counter; i++) {
+        rev_string[i] = final_string[counter - 1 - i]; 
+    }
+    rev_string[counter] = '\0';
+
+	// check if it's a palindrome or not
+	return strcmp(final_string, rev_string) == 0;
+}
+
+int main(){
+	char string[] = "0P";
+	bool ans = is_valid_Palindrome(string);
+	printf(" Palindrome: %d \n", ans);
+	return 0;
+}
