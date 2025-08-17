97a935d44c576f833ebfd406acd2d4e7cbda161e
IshanJawade <ishankjawade@gmail.com>
2025-08-17 15:19:11 -0700

    server code added in socket programming folder. with no errors

---
+++ socket_programming/server.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+// error fucntion to catch error in msg and terminate the program
+void error(const char *msg){
+	perror(msg);
+	exit(1);
+}
+
+// 2 arguments expected. 1) file name 2) port number
+int main(int argc, char *argv[]){	
+
+	// check commandline arguments
+	if(argc != 2){
+		fprintf(stderr, "Invalid arguments. Program ternimate !\n Usage: ./<filename> <port_number>\n");
+		exit(1);
+	}
+
+	int sockfd, newsockfd, portno, n;
+	char buffer[255];
+
+	struct sockaddr_in serv_addr, cli_addr; 	// contains internet address
+	socklen_t clilen;		// 32 bit datatype in in.h file
+
+	// socket Fn()	 IPv4	,	TCP		, TCP
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	// return 0 in success, return -1 in failure
+	if(sockfd < 0){
+		error("Error opening Socket !\n");
+	}
+	// clears everything in serv_addr memory location
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+
+	portno = atoi(argv[1]); // from commandline argument
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+	//				 type-casting the memory at 
+	if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){
+		error("Binding failed !");
+		// exit(1);
+	}
+
+	listen(sockfd, 5);
+	clilen = sizeof(cli_addr);
+
+	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+	if(newsockfd < 0){
+		error("Error on accept !");
+	}
+
+	while (1){
+		bzero(buffer, 255);
+		n = read(newsockfd, buffer, 255);
+		if(n<0){
+			error("Error on reading !");
+		}
+		printf("Client: %s\n", buffer);
+		bzero(buffer, 255);
+		fgets(buffer, 255, stdin);
+
+		n = write(newsockfd, buffer, strlen(buffer));
+		if(n<0){
+			error("Error on writting !\n");
+		}
+		int i = strcmp("Bye", buffer);
+		if(i==0) {
+			break;
+		}
+	}
+	close(newsockfd);
+	close(sockfd);
+	return 0;
+}
--- test.c
+++ test.c
@@ -1,62 +1,8 @@
 # include <stdio.h>
-# include <string.h>
-# include <stdlib.h>
 
-typedef struct Node {
-    int val;
-    struct Node *next;
-} Node;
-
-Node *createNode(int n);
-void printLinkedList(Node *n1);
-void freeMem(Node *n1);
-
-int main() {
-    Node *n1 = createNode(1);
-    Node *n2 = createNode(2);
-    Node *n3 = createNode(3);
-    Node *n4 = createNode(4);
-    Node *n5 = createNode(5);
-
-    n1->next = n2;
-    n2->next = n3;
-    n3->next = n4;
-    n4->next = n5;
-    // n5->next = NULL; // no nned for this line 
-
-    printLinkedList(n1);
-    freeMem(n1);
-
+int main(){
+    int a = 54, b = 85;
+    int c = a+ b;
+    printf("%d\n", c);
     return 0;
-}
-
-Node *createNode(int n){
-    Node *new_node = (Node *) malloc(sizeof(Node));
-    if(new_node == NULL){
-        printf("Memory allocation failed!");
-        exit(1);
-    }
-    new_node->val = n;
-    new_node->next = NULL;
-    return new_node;
-}
-
-void printLinkedList(Node *n1){
-    Node *cur = n1;
-    while(cur != NULL){
-        printf("%d ", cur->val);
-        cur = cur->next;
-    }
-    printf("\n");
-}
-
-void freeMem(Node *n1){
-    Node *cur = n1;
-    Node *next = NULL;
-    while(cur != NULL){
-        next = cur->next;
-        free(cur);
-        cur = next;
-    }
-    printf("All the nodes are deleted successfully");
 }
