d2785f7051aeaed5612bf5326e695a1b35ebfed3
IshanJawade <ishankjawade@gmail.com>
2025-08-18 20:46:02 -0700

    performance code added to bench threads

--- threads/threads.c
+++ threads/threads.c
@@ -1,22 +1,33 @@
 # include <stdio.h>
 # include <pthread.h>
 
-void *computation();
+void *computation(void *add);
 
 int main(){
- pthread_t thread1;
- pthread_t thread2;
+    pthread_t thread1;
+    pthread_t thread2;
 
- pthread_create(&thread1, NULL, computation, NULL);
+    long value1 = 1;
+    long value2 = 2;
 
- pthread_create(&thread2, NULL, computation, NULL);
+    // computation((void*) &value1);
+    // computation((void*) &value2);
 
- pthread_join(thread1, NULL);
- pthread_join(thread2, NULL);
+    pthread_create(&thread1, NULL, computation, (void*) &value1);
+    pthread_create(&thread2, NULL, computation, (void*) &value2);
 
- return 0;
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    return 0;
 }
 
-void *computation(){
- printf("Computation called\n");
+void *computation(void *add){
+    long sum = 0;
+    long *add_num = (long *) add;
+
+    for(long i=0; i<1000000000; i++){
+        sum = sum + *add_num;
+    }
+    printf("Add: %ld", sum);
 }
